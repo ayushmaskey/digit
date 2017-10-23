47fe77f540768e270d8dc9e7defd57ac8b68ea03
amaskey <maskey.maskey@gmail.com>
2017-10-22 19:55:38 -1000

    phone can only be submitted at ddd-ddd-dddd using regex but no error message

--- app/imports/api/contacts/contacts.js
+++ app/imports/api/contacts/contacts.js
@@ -1,3 +1,4 @@
+import { Meteor } from 'meteor/meteor';
 import { Mongo } from 'meteor/mongo';
 import { SimpleSchema } from 'meteor/aldeed:simple-schema';
 
@@ -25,19 +26,21 @@ export const ContactsSchema = new SimpleSchema({
     label: 'address',
     type: String,
     optional: false,
-    max: 20,
+    max: 50,
   },
   phone: {
     label: 'phone',
     type: String,
     optional: false,
-    max: 20,
+    max: 12,
+    regEx: /^[0-9]{3}-[0-9]{3}-[0-9]{4}$/,
   },
   email: {
     label: 'email',
     type: String,
     optional: false,
-    max: 20,
+    max: 30,
+    regEx: SimpleSchema.RegEx.Email,
   },
 });
 
